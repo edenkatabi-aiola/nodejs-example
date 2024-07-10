1c4fcd8fcc17e6e6a299f3dafdd79518891b9a10
edenkatabi <eden.katabi@aiola.com>
2024-07-10 21:01:07 +0300

    socket test

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const { createServer } = require('node:http');
+const app = express();
+const { Server } = require("socket.io");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+
+const port = 3000
+const server = createServer(app);
+const io = new Server(server, {
+  path:'/eden',
+  addTrailingSlash:false
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.get('/', (req, res) => {
+  res.send(`result: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+})
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    console.log('Message: ' + msg);
+  });
+  socket.on('binary_data', (msg) => {
+    console.log('Message: ' + msg.toString());
+  });
+  console.log('a user connected');
 });
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
